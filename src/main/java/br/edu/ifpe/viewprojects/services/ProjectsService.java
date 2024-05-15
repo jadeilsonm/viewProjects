ff49070b9f3b47bcd10581c5546cdecaf0e98961
Jadeilson Martinho <jadeilsonm17@gmail.com>
2024-05-15 18:19:57 -0300

    feat: add service layer

--- src/main/java/br/edu/ifpe/viewprojects/services/ProjectsService.java
+++ src/main/java/br/edu/ifpe/viewprojects/services/ProjectsService.java
@@ -1,12 +1,33 @@
 package br.edu.ifpe.viewprojects.services;
 
-import br.edu.ifpe.viewprojects.dao.FabricDAO;
-import br.edu.ifpe.viewprojects.dao.IGenericDAO;
+import br.edu.ifpe.viewprojects.entites.Project;
+import br.edu.ifpe.viewprojects.repositories.FabricRepository;
+import br.edu.ifpe.viewprojects.repositories.IProductRepository;
 
-public class ScholServices {
-    private IGenericDAO genericDAO;
+import java.time.LocalDate;
 
-    public ScholServices() {
-        this.genericDAO = FabricDAO.getGenericDAO();
+public class ProjectsService {
+    private IProductRepository repository;
+
+    public ProjectsService() {
+        this.repository = FabricRepository.getProductRepository();
     }
+
+    public void Add(Project p) {
+        if (p != null) {
+            if (p.getStartDate() == null)
+                p.setStartDate(LocalDate.now());
+            if (p.getEndDate() == null)
+                p.setEndDateDate(LocalDate.now().plusDays(20));
+            if (p.getUserId() == null || p.getUserId() == 0)
+                p.setUserId(1);
+
+            repository.add(p);
+        }
+    }
+
+    public void Delete(Integer id) {
+        repository.delete(id);
+    }
+
 }
