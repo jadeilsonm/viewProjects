4e4fa5f30c4d7221641e1ec6f6b2563b3d378284
Jadeilson Martinho <jadeilsonm17@gmail.com>
2024-05-15 11:01:02 -0300

    feat: add entites with build pattern

--- src/main/java/br/edu/ifpe/viewprojects/Entites/Project.java
+++ src/main/java/br/edu/ifpe/viewprojects/Entites/Project.java
@@ -1,2 +1,126 @@
-package br.edu.ifpe.viewprojects.Entites;public class Project {
+package br.edu.ifpe.viewprojects.Entites;
+
+public class Project {
+    private Integer id;
+    private String name;
+    private String description;
+    private String status;
+    private String startDate;
+    private String endDate;
+    private Integer userId;
+
+    public Project(Build build) {
+        this.name = build.name;
+        this.description = build.description;
+        this.status = build.status;
+        this.startDate = build.startDate;
+        this.endDate = build.endDate;
+        this.userId = build.userId;
+        this.id = build.id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public class Build {
+        private Integer id;
+        private String name;
+        private String description;
+        private String status;
+        private String startDate;
+        private String endDate;
+        private Integer userId;
+
+        public Build setId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public Build setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Build setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Build setStatus(String status) {
+            this.status = status;
+            return this;
+        }
+
+        public Build setStartDate(String startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public Build setEndDate(String endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+        public Build setUserId(Integer userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Project build() {
+            return new Project(this);
+        }
+    }
 }
--- src/main/java/br/edu/ifpe/viewprojects/Entites/User.java
+++ src/main/java/br/edu/ifpe/viewprojects/Entites/User.java
@@ -1,2 +1,94 @@
-package br.edu.ifpe.viewprojects.Entites;public class User {
+package br.edu.ifpe.viewprojects.Entites;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    public User(Build build) {
+        this.name = build.name;
+        this.email = build.email;
+        this.password = build.password;
+        this.role = build.role;
+        this.id = build.id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public class Build {
+        private Integer id;
+        private String name;
+        private String email;
+        private String password;
+        private String role;
+
+        public Build setId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public Build setName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Build setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Build setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Build setRole(String role) {
+            this.role = role;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
 }
